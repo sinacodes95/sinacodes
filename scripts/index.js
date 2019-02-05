a45a88cc3e4719aa66ddca09f13225fea67e3e7f
sina <sinakabki@gmail.com>
2019-02-05 15:24:53 +0000

    Skills added

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.parallax');
   M.Parallax.init(elems);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  M.Tooltip.init(elems);
+});
